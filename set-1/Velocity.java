ceff85c6ced83c820c92f415ea53bec82f2ef7d0
Sebastian Lucero-Chavez <matthewtk51@gmail.com>
2023-11-12 05:30:15 -0700

    commit to show example

---
+++ set-1/Velocity.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Velocity{
+    public static void main(String[] args){
+        Scanner scnr = new Scanner(System.in);
+
+        System.out.println("What was the initial position? ");
+        int initialPosition = scnr.nextInt();
+        System.out.println("What was the final position");
+        int finalPosition = scnr.nextInt();
+        System.out.println("What was the initial time?");
+        int initialTime = scnr.nextInt();
+        System.out.println("What was the final time?");
+        int finalTime = scnr.nextInt();
+
+        System.out.println("The velocity of the object is " + calcVelocity(initialPosition, finalPosition, initialTime, finalTime));
+        
+        scnr.close();
+    }
+    public static double calcVelocity(int initialPos, int finalPos, int initialTime, int finalTime){
+        return(finalPos - initialPos)/(finalTime - initialTime);
+    }
+}
