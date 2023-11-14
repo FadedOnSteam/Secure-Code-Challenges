1336f83d0072d2b247c9034fa60ea8701e446cdd
Javier <vazquezjavier1212@gmail.com>
2023-11-13 20:58:51 -0700

    Update Velocity.java

--- set-1/Velocity.java
+++ set-1/Velocity.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
-public class Velocity{
-    public static void main(String[] args){
+
+public class Velocity {
+    public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
 
         System.out.println("What was the initial position? ");
@@ -12,11 +13,26 @@ public class Velocity{
         System.out.println("What was the final time?");
         int finalTime = scnr.nextInt();
 
-        System.out.println("The velocity of the object is " + calcVelocity(initialPosition, finalPosition, initialTime, finalTime));
-        
+        System.out.println("The velocity of the object is "
+                + calcVelocity(initialPosition, finalPosition, initialTime, finalTime));
+                
+        printReverseRec(5);
         scnr.close();
+
     }
-    public static double calcVelocity(int initialPos, int finalPos, int initialTime, int finalTime){
-        return(finalPos - initialPos)/(finalTime - initialTime);
+
+    public static double calcVelocity(int initialPos, int finalPos, int initialTime, int finalTime) {
+        return (finalPos - initialPos) / (finalTime - initialTime);
     }
+
+    public static void printReverseRec(int n) {
+        if (n == 1) {
+            System.out.print(n);
+        } else {
+            System.out.print(n + ", ");
+            printReverseRec(--n);
+        }
+
+    }
+
 }
