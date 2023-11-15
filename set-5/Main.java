edf0f547a635c439d4632c7dcd29c62ce7370f8b
Javier <vazquezjavier1212@gmail.com>
2023-11-14 23:33:18 -0700

    Create Main.java

---
+++ set-5/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.setName("John");
+        s.setAge(18);
+        System.out.println(s.getName());
+        System.out.println(s.getAge());
+    }
+}
+
+class Student {
+    public String name;
+    public int age;
+
+    public void setName(String newName) {
+        name = newName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int newAge) {
+        age = newAge;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
